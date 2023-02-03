dfd552e8cf61106ba550e1e97aba3984cdb32a40
Stanislav Janů <s@janu.software>
2023-02-03 16:19:23 +0100

    custom presets

--- src/pdf-options/presets/my-preset.sample.ts
+++ src/pdf-options/presets/my-preset.sample.ts
@@ -2,13 +2,25 @@
  * @see https://github.com/puppeteer/puppeteer/blob/v5.3.1/docs/api.md#pagepdfoptions
  */
 
-import { PDFOptions } from 'puppeteer'
+import { PDFMargin, PDFOptions } from 'puppeteer'
+
+const zeroMargin: PDFMargin = {
+  top: '0mm',
+  bottom: '0mm',
+  left: '0mm',
+  right: '0mm',
+}
 
 export const PresetPDFOptions: { [key: string]: PDFOptions } = {
-  MYA4: {
+  A4Full: {
     format: 'a4',
+    margin: zeroMargin,
+    printBackground: true,
   },
-  /**
-   * add your options
-   */
+  A4LandscapeFull: {
+    format: 'a4',
+    landscape: true,
+    margin: zeroMargin,
+    printBackground: true,
+  },
 }
