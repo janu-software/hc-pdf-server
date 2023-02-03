f1fb63e61ca693d070f06df9b4220bc4dd087761
Stanislav Janů <s@janu.software>
2023-02-03 16:19:53 +0100

    upgrade server listening depracations

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import { app } from './app'
 import { SERVER_PORT, SERVER_ADDRESS } from './config'
+import { FastifyListenOptions } from 'fastify/types/instance'
 ;(async () => {
   const server = await app()
-  server.listen(SERVER_PORT, SERVER_ADDRESS, (err, address) => {
+  const options: FastifyListenOptions = {
+    port: SERVER_PORT,
+    host: SERVER_ADDRESS,
+  }
+  server.listen(options, (err, address) => {
     if (err) {
       console.error(err)
       process.exit(1)
